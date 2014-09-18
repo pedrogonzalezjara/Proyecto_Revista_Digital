c676e6cba530be01cf1fc1738a14858280277fd6
Pedro Gonzalez <pdro.gonzalez@gmail.com>
2014-09-18 00:50:04 -0300

    Revert "Update TestController.php"
    This reverts commit 3257bb8717b0e2797549513b7c13869e2dfe63cb.

--- Revista_Digital/app/controllers/TestController.php
+++ Revista_Digital/app/controllers/TestController.php
@@ -108,7 +108,7 @@ class TestController extends BaseController {
                                else
                                    $genero=0;
                                $data->genero=$genero;
-                               $data->direccion=$objeto->comuna['comuna'];
+                               $data->direccion="comuna";
                                $perfil = Departamentos::where('nombre','=',$objeto->departamento['departamento'])->first(array("id"));  
                                $data->departamento_fk= $perfil;
                                $data->save();
@@ -157,7 +157,7 @@ class TestController extends BaseController {
                                else
                                    $genero=0;
                                $data->genero=$genero;
-                                $data->direccion=$data->direccion=$objeto->comuna['comuna'];
+                                $data->direccion="prueba";
                                $perfil = Departamentos::where('nombre','=',$objeto->departamento['departamento'])->first(array("id")); 
                                $data->departamento_fk=$perfil;
                                $data->save();
